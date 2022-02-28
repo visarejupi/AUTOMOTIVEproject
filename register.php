c266537fda437c79cc0efff317488a50bdfce77d
nx35602 <nx35602@ubt-uni.net>
2022-02-28 15:25:01 +0100

    validimi ne backend

--- register.php
+++ register.php
@@ -1,32 +1,32 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "automotive";
+include "includes/db.php";
 
 $name = $_GET["username"];
 $email = $_GET["email"];
 $user_password = $_GET["password"];
-$salt = base64_encode(random_bytes(10));
-$salted_hash = hash("sha256", $user_password.$salt, false);
-try {
-  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-  // set the PDO error mode to exception
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $sql = "INSERT INTO users (name, email, salted_hash, salt, privileges)
-  VALUES ('$name', '$email', '$salted_hash', '$salt', 1)";
-  // use exec() because no results are returned
-  $conn->exec($sql);
-  echo "User Registered Successfuly";
-} catch(PDOException $e) {
-  echo $sql . "<br>" . $e->getMessage();
+
+if (filter_var($email, FILTER_VALIDATE_EMAIL) == false || strlen($user_password)<8) {
+  header("Location:/signup.php");
 }
+else {
+  $salt = base64_encode(random_bytes(10));
+  $salted_hash = hash("sha256", $user_password.$salt, false);
+  try {
+    $sql = "INSERT INTO users (name, email, salted_hash, salt, privileges)
+    VALUES ('$name', '$email', '$salted_hash', '$salt', 1)";
 
-$conn = null;
+    $conn->exec($sql);
+    setcookie("username", $name, time() + (86400), "/");
+    setcookie("pass_hash", hash("sha256", $salted_hash, false), time() + (86400), "/");
+  }
+  catch(PDOException $e) {
+    echo $sql . "<br>" . $e->getMessage();
+  }
 
-// create a cookie
+  $conn = null;
 
-header('Location: '."/index.php");
+  header('Location: '."/index.php");
+}
 
 exit();
 ?>
